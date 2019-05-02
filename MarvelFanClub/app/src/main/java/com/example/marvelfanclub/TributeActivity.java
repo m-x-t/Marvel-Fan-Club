77ca83a3e1e3735eebf1a4399dc4279ba159d398
Xfairley717141 <46569218+Xfairley717141@users.noreply.github.com>
2019-05-01 22:20:56 -0500

    Add files via upload

--- MarvelFanClub/app/src/main/java/com/example/marvelfanclub/TributeActivity.java
+++ MarvelFanClub/app/src/main/java/com/example/marvelfanclub/TributeActivity.java
@@ -2,7 +2,10 @@ package com.example.marvelfanclub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.EditText;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 
 
 public class TributeActivity extends AppCompatActivity {
@@ -11,5 +14,19 @@ public class TributeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tribute);
+
+        ImageView ivProfile;
+        HorizontalScrollView hsvCoverPhoto;
+        ImageView ivCoverPhoto;
+        EditText etUsername;
+        ScrollView svContent;
+
+        ivProfile = findViewById(R.id.ivProfile);
+        hsvCoverPhoto = findViewById(R.id.hsvCoverPhoto);
+        ivCoverPhoto = findViewById(R.id.ivCoverPhoto);
+        etUsername= findViewById(R.id.etUsername);
+        svContent = findViewById(R.id.svContent);
+
+
     }
 }
